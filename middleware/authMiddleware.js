ded3e5bb24239403511dca0e76d364c494bdc971
Pratyaksha37 <pratyakshashastri123@gmail.com>
2025-09-29 15:00:48 +0530

    .

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,15 +1,36 @@
+// require('dotenv').config();
+
+// const SECRET_KEY = process.env.SHIPPING_SECRET_KEY;
+
+// function verifySecret(req, res, next) {
+//   const key = req.header('SHIPPING_SECRET_KEY');
+//   if (!key) {
+//     return res.status(403).json({ error: 'SHIPPING_SECRET_KEY is missing or invalid' });
+//   }
+//   if (key !== SECRET_KEY) {
+//     return res.status(403).json({ error: 'Failed to authenticate SHIPPING_SECRET_KEY' });
+//   }
+//   next();
+// }
+
+// module.exports = { verifySecret };
+
+// middleware/authMiddleware.js
 require('dotenv').config();
 
 const SECRET_KEY = process.env.SHIPPING_SECRET_KEY;
 
 function verifySecret(req, res, next) {
   const key = req.header('SHIPPING_SECRET_KEY');
+
   if (!key) {
-    return res.status(403).json({ error: 'SHIPPING_SECRET_KEY is missing or invalid' });
+    return res.status(403).json({ error: 'apiauthkey is missing or invalid' });
   }
+
   if (key !== SECRET_KEY) {
-    return res.status(403).json({ error: 'Failed to authenticate SHIPPING_SECRET_KEY' });
+    return res.status(403).json({ error: 'Failed to authenticate apiauthkey' });
   }
+
   next();
 }
 
